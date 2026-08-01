d89b0c86e1f54dab539e123726ff44fae0f31ee1
lambrech <christian_lambrecht@agilent.com>
2026-08-01 20:28:46 +0200

    Register ContentService and UpdateService in DI

--- src/HalstuchLernApp/Program.cs
+++ src/HalstuchLernApp/Program.cs
@@ -8,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ContentService>();
+builder.Services.AddScoped<UpdateService>();
 
 await builder.Build().RunAsync();
