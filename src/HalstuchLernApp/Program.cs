3e8a36b483ae61825d5bd892709481dabe58d154
lambrech <christian_lambrecht@agilent.com>
2026-08-01 20:30:17 +0200

    Add missing using for Services in Program.cs

--- src/HalstuchLernApp/Program.cs
+++ src/HalstuchLernApp/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using HalstuchLernApp;
+using HalstuchLernApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
